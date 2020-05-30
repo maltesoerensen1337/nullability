cc36acf6c4e9f6b76112cdc28cd260139626ff33
msoerens <malte.soerensen@otto.de>
2020-05-30 02:25:21 +0200

    keep sonarqube plugins over restarts

--- sonarqube/start-sonar.sh
+++ sonarqube/start-sonar.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-SQ_DATA=$(pwd)/data
+SQ_DATA=$(pwd)/volumes/data
+SQ_EXTENSIONS=$(pwd)/volumes/extensions
 mkdir -p $SQ_DATA
+mkdir -p $SQ_EXTENSIONS
 docker run -d --name sonarqube \
 	-v $SQ_DATA:/opt/sonarqube/data \
+	-v $SQ_EXTENSIONS:/opt/sonarqube/extensions \
 	-p 9000:9000 \
 	sonarqube:8.3-community
